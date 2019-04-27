f3afa498c145b1042061b9a0204763202ccc23a2
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-27 21:54:18 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :
+  
   
   # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
