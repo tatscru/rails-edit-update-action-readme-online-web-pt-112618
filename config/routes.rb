9776bff978604d601a270201dcb0d66527ce12fc
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-27 22:04:01 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # resources :articles [:index, :show, :new, :edit, :update]
+  resources :articles [:index, :show, :new, :edit, :update]
   
   
-  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch 'articles/:id', to: 'articles#update'
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
