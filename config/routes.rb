008dd53a6edf5c33fc5ff3f4ba28ae799d1846f0
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-27 21:54:49 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles [:]
+  resources :articles [:index, :new, :edit, ]
   
   
   # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
