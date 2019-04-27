ccaeb0795f1c328079fdcab77671755bd9f64457
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-27 22:55:13 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,8 @@ class ArticlesController < ApplicationController
   end 
   
   def update 
+    raise params.inspect 
+    
     @article = Article.find(params[:id])
     
     @article.update(title: params[:title], description: params[:description])
